f03aaf4491f2d59a21eb50c5ebb59ff6f9064134
Jeffery Kryzewski <jeffkryzewski@Jefferys-Air.lan>
2022-07-24 17:26:35 -0400

    update 2

--- javascript.js
+++ javascript.js
@@ -4,33 +4,43 @@ const symbols = [
 "7", // 2
 "Plum", // 3
 "Water Melon",// 4
- "Lemon", // 5
+"Lemon", // 5
  "Cherry", // 6
  "Orange", // 7
  "Bar", // 8
 ];
 
-console.log(symbols);
-
-const chooseRandom = (symbols, num) => {
-    const result = [];
+const chooseRandom1 = (symbols, num) => {
+    const result1 = [];
     for(let i = 0; i < num; i++ ){
        const random = Math.floor(Math.random() * symbols.length);
-
        console.log(random);
-
-    //    if(result.indexOf(symbols[random]) !== -1){
-    //       continue;
-    //    };
-       result.push(symbols[random]);
-
-       
-       
+       result1.push(symbols[random]);  
     }; 
-    console.log(result,"line 30");
-    return result;
+    return result1;
  };
- console.log(chooseRandom(symbols, 3));
+ console.log(chooseRandom1(symbols, 1), "line 33");
 
+ const chooseRandom2 = (symbols, num) => {
+    const result2 = [];
+    for(let i = 0; i < num; i++ ){
+       const random = Math.floor(Math.random() * symbols.length);
+       console.log(random);
+       result2.push(symbols[random]);  
+    }; 
+    return result2;
+ };
+ console.log(chooseRandom2(symbols, 1), "line 33");
+
+ const chooseRandom3 = (symbols, num) => {
+    const result3 = [];
+    for(let i = 0; i < num; i++ ){
+       const random = Math.floor(Math.random() * symbols.length);
+       console.log(random);
+       result3.push(symbols[random]);  
+    }; 
+    return result3;
+ };
+ console.log(chooseRandom3(symbols, 1), "line 33");
 
  
