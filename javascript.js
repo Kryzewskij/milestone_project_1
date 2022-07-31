8b05ed5f816f6d3da7bc33058742f639eb420f57
Jeffery Kryzewski <jeffkryzewski@Jefferys-Air.lan>
2022-07-31 12:00:27 -0400

    reset button working

--- javascript.js
+++ javascript.js
@@ -9,58 +9,32 @@ const symbols = [
  "Orange", // 7
  "Bar", // 8
 ];
-
-   var slot1 = chooseRandom(symbols, 1);
-   var slot2 = chooseRandom(symbols, 1);
-   var slot3 = chooseRandom(symbols, 1);
+   
+   
    const playButton = document.getElementById("playButton");
    const resetButton = document.getElementById("resetButton")
-   
-
-   //document.getElementById("playButton").addEventListener("click", clickPlay);
 
    playButton.addEventListener("click", clickPlay); 
 
    function clickPlay() {
+      let slot1 = chooseRandom(symbols, 1);
+      let slot2 = chooseRandom(symbols, 1);
+      let slot3 = chooseRandom(symbols, 1);
       document.getElementById("slot1").innerHTML = slot1;
       document.getElementById("slot2").innerHTML = slot2;
       document.getElementById("slot3").innerHTML = slot3;
-      };
-      
+      }; 
    function chooseRandom(symbols) {
       var result = [];
       var random = Math.floor(Math.random() * symbols.length);
       result.push(symbols[random]);
       return result;
    };
-   resetButton.addEventListener("click", reset)
+
+   resetButton.addEventListener("click", reset);
+
     function reset() {
-        if(slot1 !== "?" && slot2!== "?" && slot3 !== "?") {
-            //message.innerHTML = "";
-            chooseRandom();
-        }
+         document.getElementById("slot1").innerHTML = "Slot 1";
+         document.getElementById("slot2").innerHTML = "Slot 2";
+         document.getElementById("slot3").innerHTML = "Slot 3";  
     }
-
-
-   
-
-  
-
-
-   
-
-   
-
-   
-
-   
-
-
-
-
-
-
-  
-
-
- 
